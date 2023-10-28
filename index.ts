952b7d1281085b6473fcbb7a19c6a2f0a31317f0
koszczapinski <koszczapinski@gmail.com>
2023-10-28 02:28:58 +0200

    Returned bun

--- index.ts
+++ index.ts
@@ -1,10 +1,1 @@
-import { getTask, getToken, sendAnswer } from "./common";
-
-const token = await getToken("helloapi");
-console.log("Token:", token);
-
-const task = await getTask(token);
-console.log("Task:", task);
-
-const answer = await sendAnswer(token, "Hello!");
-console.log("Answer:", answer);
+console.log("Hello via Bun!");
