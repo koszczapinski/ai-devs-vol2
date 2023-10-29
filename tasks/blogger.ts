97b3ef24aa45afe15be481938f4240aeca522a58
koszczapinski <koszczapinski@gmail.com>
2023-10-29 02:31:49 +0200

    Changed model in blogger task

--- tasks/blogger.ts
+++ tasks/blogger.ts
@@ -8,7 +8,9 @@ const { blog } = await getTask<TaskResponse & { blog: string[] }>(token);
 const sectionsContent: string[] = [];
 
 for (const title of blog) {
-  const test = await openAICompletion(title, BLOGGER_SYSTEM_PROMPT);
+  const test = await openAICompletion(title, BLOGGER_SYSTEM_PROMPT, {
+    model: "gpt-4",
+  });
   sectionsContent.push(test.choices[0].message.content);
 }
 
