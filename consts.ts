3f1d4982e9281eb06ea5179d7f91e4dbcc5b066a
koszczapinski <koszczapinski@gmail.com>
2023-10-30 02:46:36 +0100

    System prompt typo fix

--- consts.ts
+++ consts.ts
@@ -7,7 +7,7 @@ export const BLOGGER_TASK_SYSTEM_PROMPT =
   "Wygeneruj treść sekcji bloga na podstawie tytułu podanego przez użytkownika. Maksymalnie 5 zdań.";
 
 export const LIAR_TASK_USER_QUESTION = "What is the capital of Poland?";
-export const LIAR_TASK_GUARDRAILS_SYSTEM_PROMPT = `Your will check if the answer to a provided question is correct. If it is, return 'YES', if it is not, return 'NO' and nothing else.
+export const LIAR_TASK_GUARDRAILS_SYSTEM_PROMPT = `You will check if the answer to a provided question is correct. If it is, return 'YES', if it is not, return 'NO' and nothing else.
 ###
 Question: ${LIAR_TASK_USER_QUESTION}
 ###
