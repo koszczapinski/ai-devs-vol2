7ead3d812ef608584be9f3e9b4a800bff4312322
koszczapinski <koszczapinski@gmail.com>
2023-10-30 02:43:11 +0100

    Added task code

--- consts.ts
+++ consts.ts
@@ -3,5 +3,12 @@ export const OPENAI_MODERTATIONS_API_ENDPOINT =
 export const OPENAI_COMPLETIONS_API_ENDPOINT =
   "https://api.openai.com/v1/chat/completions";
 
-export const BLOGGER_SYSTEM_PROMPT =
+export const BLOGGER_TASK_SYSTEM_PROMPT =
   "Wygeneruj treść sekcji bloga na podstawie tytułu podanego przez użytkownika. Maksymalnie 5 zdań.";
+
+export const LIAR_TASK_USER_QUESTION = "What is the capital of Poland?";
+export const LIAR_TASK_GUARDRAILS_SYSTEM_PROMPT = `Your will check if the answer to a provided question is correct. If it is, return 'YES', if it is not, return 'NO' and nothing else.
+###
+Question: ${LIAR_TASK_USER_QUESTION}
+###
+  `;
--- tasks/blogger.ts
+++ tasks/blogger.ts
@@ -1,5 +1,5 @@
 import { getTask, getToken, openAICompletion, sendAnswer } from "../common";
-import { BLOGGER_SYSTEM_PROMPT } from "../consts";
+import { BLOGGER_TASK_SYSTEM_PROMPT } from "../consts";
 import { TaskResponse } from "../types";
 
 const token = await getToken("blogger");
@@ -8,7 +8,7 @@ const { blog } = await getTask<TaskResponse & { blog: string[] }>(token);
 const sectionsContent: string[] = [];
 
 for (const title of blog) {
-  const data = await openAICompletion(title, BLOGGER_SYSTEM_PROMPT, {
+  const data = await openAICompletion(title, BLOGGER_TASK_SYSTEM_PROMPT, {
     model: "gpt-4",
   });
   sectionsContent.push(data.choices[0].message.content);
