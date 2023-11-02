2bd8c835d5b83712a5e1bfadfcf24c3b41c4e44c
koszczapinski <koszczapinski@gmail.com>
2023-11-02 02:36:39 +0100

    Added code for embedding task

--- consts.ts
+++ consts.ts
@@ -2,6 +2,8 @@ export const OPENAI_MODERTATIONS_API_ENDPOINT =
   "https://api.openai.com/v1/moderations";
 export const OPENAI_COMPLETIONS_API_ENDPOINT =
   "https://api.openai.com/v1/chat/completions";
+export const OPENAI_EMBEDDINGS_API_ENDPOINT =
+  "https://api.openai.com/v1/embeddings";
 
 export const BLOGGER_TASK_SYSTEM_PROMPT =
   "Wygeneruj treść sekcji bloga na podstawie tytułu podanego przez użytkownika. Maksymalnie 5 zdań.";
