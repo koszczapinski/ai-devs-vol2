433d8c538f705d6535cb1ec3710a46ed4163bb45
Krzysztof Oszczapiński <koszczapinski@gmail.com>
2023-11-05 19:05:19 +0100

    Task/functions (#7)
    * Improved whisper task logic
    
    * Added code for task 'functions'

--- tasks/whisper.ts
+++ tasks/whisper.ts
@@ -5,15 +5,24 @@ import { TaskResponse } from "../types";
 const DOWNLOADED_FILE_NAME = "file-for-transcription.mp3";
 
 const token = await getToken("whisper");
-const { msg } = await getTask<TaskResponse & {hint: string}>(token);
+const { msg } = await getTask<TaskResponse & { hint: string }>(token);
+
+if (!msg) {
+  throw new Error("No message found in task response");
+}
 
 const file = Bun.file(DOWNLOADED_FILE_NAME);
 const fileExist = await file.exists();
 if (fileExist) {
-    await unlink(DOWNLOADED_FILE_NAME)
+  await unlink(DOWNLOADED_FILE_NAME);
 }
 
-const fileUrl = msg.match(/(https?:\/\/[^\s]+)/g)[0];
+const fileUrlMatch = msg.match(/(https?:\/\/[^\s]+)/g);
+if (!fileUrlMatch) {
+  throw new Error("No file URL found in task message");
+}
+const fileUrl = fileUrlMatch[0];
+
 const response = await fetch(fileUrl);
 await Bun.write(DOWNLOADED_FILE_NAME, response);
 
