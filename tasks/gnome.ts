162a2937b8b2ac135fa4aff997142ba0b0d4754e
Krzysztof Oszczapiński <koszczapinski@gmail.com>
2023-11-16 23:09:15 +0100

    Task/gnome (#16)
    * Added base for gnome task
    
    * Added solved gnome task
    
    * Removed unnecessary console log

---
+++ tasks/gnome.ts
@@ -0,0 +1,28 @@
+import OpenAI from "openai";
+import { getTask, getToken, sendAnswer } from "../common";
+import { TaskResponse } from "../types";
+
+const openai = new OpenAI();
+
+const token = await getToken("gnome");
+const { msg: text, url } = await getTask<TaskResponse & { url: string }>(token);
+
+const response = await openai.chat.completions.create({
+  model: "gpt-4-vision-preview",
+  messages: [
+    {
+      role: "user",
+      content: [
+        { type: "text", text },
+        {
+          type: "image_url",
+          image_url: {
+            url,
+          },
+        },
+      ],
+    },
+  ],
+});
+
+await sendAnswer(token, response.choices[0].message.content);
